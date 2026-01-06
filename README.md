874e63d86342f54d64658594aa8b6d8894d07d92
Antoni Szymański <antoni.szymanski+github@proton.me>
2026-01-06 23:29:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# git-credential-store-pathprefix
+
+`git-credential-store-pathprefix` is a Git credential helper that provides credentials based on the repo path prefix. It's heavily inspired by [git-credential-store-path](https://github.com/kastelo-labs/git-credential-store-path).
---
+++ src/main.rs
@@ -0,0 +1,112 @@
+// SPDX-FileCopyrightText: 2026 Antoni Szymański
+// SPDX-License-Identifier: MPL-2.0
+
+use clap::{Parser, Subcommand};
+use gitcredential::GitCredential;
+use snafu::{ResultExt, Snafu};
+use std::{
+    env,
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::PathBuf,
+};
+use url::Url;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Return a matching credential, if any exists.
+    Get,
+    /// Store the credential.
+    Store,
+    /// Remove matching credentials, if any, from the storage.
+    Erase,
+}
+
+#[derive(Debug, Snafu)]
+#[snafu(context(suffix(Ctx)))]
+#[non_exhaustive]
+enum Error {
+    #[snafu(display("Failed to parse credential from stdin"))]
+    Parse { source: gitcredential::FromReaderError },
+    #[snafu(display("Failed to lookup credential"))]
+    Lookup { source: LookupError },
+    #[snafu(display("Failed to write credential to stdout"))]
+    Write { source: io::Error },
+}
+
+#[snafu::report]
+fn main() -> Result<(), Error> {
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Get => {
+            let input = GitCredential::from_reader(io::stdin()).context(ParseCtx)?;
+            if let Some(output) = lookup_credential(&input).context(LookupCtx)? {
+                output.to_writer(io::stdout()).context(WriteCtx)?;
+            }
+        }
+        Commands::Store | Commands::Erase => {}
+    }
+    Ok(())
+}
+
+#[derive(Debug, Snafu)]
+#[snafu(context(suffix(Ctx)))]
+#[non_exhaustive]
+enum LookupError {
+    #[snafu(display("Failed to get the home directory"))]
+    GetHomeDir,
+    #[snafu(display("Failed to open .git-credentials file"))]
+    OpenGitCredentials { source: io::Error, path: PathBuf },
+    #[snafu(display("Failed to read line from input reader"))]
+    ReadLine { source: io::Error },
+    #[snafu(display("Failed to parse URL: {input:?}"))]
+    InvalidUrl { source: url::ParseError, input: String },
+}
+
+fn lookup_credential(gc: &GitCredential) -> Result<Option<GitCredential>, LookupError> {
+    let path = env::home_dir().ok_or(LookupError::GetHomeDir)?.join(".git-credentials");
+    let file = match File::open(&path) {
+        Ok(v) => v,
+        Err(e) => {
+            if e.kind() == io::ErrorKind::NotFound {
+                return Ok(None);
+            }
+            return Err(LookupError::OpenGitCredentials { source: e, path });
+        }
+    };
+    let buf_reader = BufReader::new(file);
+    for line in buf_reader.lines() {
+        let line = line.context(ReadLineCtx)?;
+        let url = Url::parse(&line).context(InvalidUrlCtx { input: line })?;
+        if gc.protocol.as_deref() != Some(url.scheme()) && gc.host.as_deref() != url.host_str() {
+            continue;
+        }
+        if let (Some(expected), Some(actual)) = (
+            gc.username.as_deref(), //
+            Some(url.username()).filter(|s| !s.is_empty()),
+        ) && expected != actual
+        {
+            continue;
+        }
+        if let (Some(expected), actual) = (gc.path.as_deref(), trim_prefix(url.path(), "/"))
+            && !expected.starts_with(actual)
+        {
+            continue;
+        }
+        return Ok(Some(GitCredential::from_url(&url)));
+    }
+    Ok(None)
+}
+
+#[inline]
+fn trim_prefix<'a>(s: &'a str, prefix: &'a str) -> &'a str {
+    s.strip_prefix(prefix).unwrap_or(s)
+}
